daff1d54344d71f80a9e1bbe56ca0db07328d9da
BoDee Angus <bodee.angus@gmail.com>
2023-03-14 13:49:47 -0600

    trying to fix import errors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/header';
+import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Pricing from '../components/Pricing';
