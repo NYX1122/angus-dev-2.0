30cf68595228608406de2f96f4e3d2ebbfcac0d5
BoDee Angus <bodee.angus@gmail.com>
2023-04-04 21:50:55 -0600

    finished light mode styling on contact section

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,7 +2,7 @@ import SectionTitle from './infracomponents/SectionTitle';
 
 import styles from './Contact.module.css';
 
-export default function Contact() {
+export default function Contact({ lightMode }) {
   const formItemsArray = [
     { id: 'firstName', text: 'First Name:' },
     { id: 'lastName', text: 'Last Name:' },
@@ -12,15 +12,39 @@ export default function Contact() {
     { id: 'message', text: 'Message:' },
   ];
   return (
-    <div className={styles.outerContainer} id='Contact'>
-      <SectionTitle content='Contact Us' />
-      <div className={styles.formContainer}>
+    <div
+      className={
+        lightMode
+          ? `${styles['outerContainer']} ${styles['light']}`
+          : styles.outerContainer
+      }
+      id='Contact'
+    >
+      <SectionTitle content='Contact Us' lightMode={lightMode} />
+      <div
+        className={
+          lightMode
+            ? `${styles['formContainer']} ${styles['light']}`
+            : styles.formContainer
+        }
+      >
         {formItemsArray.map((item) => (
-          <div key={item.id} className={styles.formItemOuterContainer}>
+          <div
+            key={item.id}
+            className={
+              lightMode
+                ? `${styles['formItemOuterContainer']} ${styles['light']}`
+                : styles.formItemOuterContainer
+            }
+          >
             <h3>{item.text}</h3>
             <textarea
               rows={item.text === 'Message:' ? 5 : 1}
-              className={styles.formItemFieldContainer}
+              className={
+                lightMode
+                  ? `${styles['formItemFieldContainer']} ${styles['light']}`
+                  : styles.formItemFieldContainer
+              }
             ></textarea>
           </div>
         ))}
